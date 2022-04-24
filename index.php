f9b5303f37edb8a451c731d7b6b86ffa4e0e7111
Vadym <103644356+l1nkooln@users.noreply.github.com>
2022-04-24 19:20:33 +0300

    Site

---
+++ index.php
@@ -0,0 +1,507 @@
+
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>L1T - L1nkoln Site</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD. The purpose of this project is to accurately emulate the PlayStation 3 in its entirety with the power of reverse engineering and community collaboration.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, home">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8" />
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<link rel="icon" type="image/png" href="/img/meta/favicon/16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/128.png" sizes="128x128" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/196.png" sizes="196x196" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/57.png" sizes="57x57" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/60.png" sizes="60x60" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/72.png" sizes="72x72" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/76.png" sizes="76x76" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/114.png" sizes="114x114" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/120.png" sizes="120x120" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/144.png" sizes="144x144" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/152.png" sizes="152x152" />
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta property="og:title" content="RPCS3 - The PlayStation 3 Emulator" />
+<meta property="og:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+<meta name="twitter:title" content="RPCS3 - The PlayStation 3 Emulator">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v7" />
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7" />
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v8" />
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v53" />
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v7" />
+<script data-cfasync="false" src='/lib/js/jquery.js'></script>
+<script data-cfasync="false" src='/lib/js/dark.js?v8'></script>
+<script data-cfasync="false" src='/lib/js/main.js'></script>
+<script data-cfasync="false" src='/lib/js/jscookie.js'></script>
+<script data-cfasync="false" src='/lib/js/particles.js'></script>
+<script data-cfasync="false" src='/lib/js/particles-sel.js'></script>
+<noscript>
+<style>
+/*
+General:
+- Elements that fade in are not visible by default
+*/
+.fade-up-onstart {
+	display:inherit;
+}
+.fade-in-onload {
+	display:inherit;
+}
+/*
+Index:
+- Center index contents
+*/
+.landing-con-container {
+	top:50%;
+	margin-top:-139px;
+}
+/*
+Quickstart:
+- Remove dimming
+- Do not block interaction outside the sidebar
+- Slightly reduce the width of the sidebar
+*/
+.toggle-navsidebar {
+	background:none;
+	width:auto;
+}
+.sidebar-con-anim {
+	width:300px;
+}
+</style>
+</noscript>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous" type="1bec7429fbf76b0b24b30ac3-text/javascript"></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload" />
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload" />
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload" />
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload" />
+<div class='menu-con-release'>
+<span>Website Release Preview 2021.12.05</span>
+</div>
+<div class='mini-menu-con-dimmer'>
+<div class='mini-menu-con-container'>
+<div class='mini-menu-ico-logo'>
+</div>
+<div class='mini-menu-tx1-block'>
+<div class='mini-menu-emp-block'>
+</div>
+<h2>No Piracy.</h2>
+</div>
+<div class='mini-menu-tx2-block'>
+<p>
+We do not condone piracy of any kind. <u>Asking</u> for, <u>providing</u> or <u>discussing</u> illegal download links is <b>not allowed</b> in our communities.<br>
+<br>
+Purchasing legitimate game copies, through the <u>PlayStation Store</u> or through acquiring game discs, and using those copies with RPCS3 is the best way to ensure you will have a clean copy that will work with the emulator. You can use your <b>legal</b> copies with RPCS3 by following the instructions in our <a href="/quickstart" target="_blank">Quickstart</a> guide.
+</p>
+</div>
+<div class="mini-menu-btn-agree">
+<h2>I Understand</h2>
+</div>
+</div>
+</div>
+<div class='menu-con-message fade-out-onload'>
+<div class='menu-tx1-message toggle-message fade-out-onload'>
+<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+</div>
+</div>
+<div class="window-con-theme">
+<div class="window-bg-theme">
+</div>
+<div class="theme-con-spinner">
+<div class="theme-ico-spinner">
+</div>
+</div>
+<div class="theme-tx1-description pulsate">
+<span>This feature is currently not yet available</span>
+</div>
+<div class="wavebar-con-container-theme">
+<div class="wavebar-con-wrap">
+<div class="wavebar-svg-object">
+</div>
+<div class="wavebar-svg-object">
+</div>
+</div>
+</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+<div class="settings-menu-ico-darkmode">
+</div>
+<div class="settings-menu-btn-button btn-darkmode">
+<span>Dark Theme - On/Off</span>
+</div>
+</div>
+
+</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+<div class="menu-con-backdrop darkmode-menu">
+</div>
+<div class="menu-btn-settings toggle-settings">
+</div>
+<div class="menu-con-outer">
+<div class="menu-con-inner">
+<div class="menu-con-logo">
+<a href='/'>
+<div class="menu-ico-logo">
+</div>
+</a>
+</div>
+<div class="mobile-menu-btn-open toggle-mobilemenu">
+</div>
+<div class="mobile-menu-btn-darkmode toggle-darkmode">
+</div>
+
+
+</div>
+</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+<div class="menu-con-backdrop-2">
+</div>
+<div class='mobile-menu-con-scroll'>
+<div class="mobile-menu-con-outter">
+<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+<div class='mobile-menu-tx1-group'>
+<span>Main</span>
+</div>
+<div class='mobile-menu-con-divider'>
+</div>
+<a href="/">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Home</span>
+</div>
+</a>
+<a href="/blog">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Blog</span>
+</div>
+</a>
+<a href="/about">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>About</span>
+</div>
+</a>
+<a href="/compatibility">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Compatibility</span>
+</div>
+</a>
+<a href="/download">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Download</span>
+</div>
+</a>
+<a href="/quickstart">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Quickstart</span>
+</div>
+</a>
+<a href="/roadmap">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Roadmap</span>
+</div>
+</a>
+<a href="/faq">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>FAQs</span>
+</div>
+</a>
+<a href="https://forums.rpcs3.net" target="_blank">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Forum</span>
+</div>
+</a>
+<a href='https://wiki.rpcs3.net'>
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Wiki</span>
+</div>
+</a>
+<div class='mobile-menu-tx1-group'>
+<span>Internal</span>
+</div>
+<div class='mobile-menu-con-divider'>
+</div>
+<a href="/branding">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Branding</span>
+</div>
+</a>
+<a href="/disclaimer">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Disclaimer</span>
+</div>
+ </a>
+<div class='mobile-menu-tx1-group'>
+<span>External</span>
+</div>
+<div class='mobile-menu-con-divider'>
+</div>
+<a href="https://github.com/rpcs3" target="_blank">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>GitHub</span>
+</div>
+</a>
+<a href="https://discord.me/RPCS3" target="_blank">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Discord</span>
+</div>
+</a>
+<a href="https://www.youtube.com/c/RPCS3_emu/videos" target="_blank">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>YouTube</span>
+</div>
+</a>
+<div class='mobile-menu-tx1-group'>
+<span>Support</span>
+</div>
+<div class='mobile-menu-con-divider'>
+</div>
+<a href="https://www.patreon.com/Nekotekina">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Patreon</span>
+</div>
+</a>
+<a href="/alipay">
+<div class='mobile-menu-btn-icon'>
+</div>
+<div class='mobile-menu-btn-select'>
+<span>Alipay</span>
+</div>
+</a>
+<div class='mobile-menu-btn-spacer'>
+</div>
+</div>
+</div>
+</div>
+</div><script type="1bec7429fbf76b0b24b30ac3-text/javascript">
+</script>
+<div class="video-con-dimmer toggle-video-1">
+<div class="menu-btn-close close-video stop-video">
+<div class="menu-tx1-tooltip">
+<span>Close</span>
+</div>
+</div>
+<div class="video-con-viewport">
+<div class="video-img-viewport" style="background: url(/img/videos/1.jpg) no-repeat center; background-size: cover;">
+<div class="video-ico-viewport"></div>
+</div>
+<iframe id="video-1" data-src="https://www.youtube.com/embed/gIcsUr7W00k?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+</div>
+</div>
+<script type="1bec7429fbf76b0b24b30ac3-text/javascript">
+</script>
+<div class="video-con-dimmer toggle-video-2">
+<div class="menu-btn-close close-video stop-video">
+<div class="menu-tx1-tooltip">
+<span>Close</span>
+</div>
+</div>
+<div class="video-con-viewport">
+<div class="video-img-viewport" style="background: url(/img/videos/2.jpg) no-repeat center; background-size: cover;">
+<div class="video-ico-viewport"></div>
+</div>
+<iframe id="video-2" data-src="https://www.youtube.com/embed/60lq9pXzDic?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+</div>
+</div>
+<script type="1bec7429fbf76b0b24b30ac3-text/javascript">
+</script>
+<div class="video-con-dimmer toggle-video-3">
+<div class="menu-btn-close close-video stop-video">
+<div class="menu-tx1-tooltip">
+<span>Close</span>
+</div>
+</div>
+<div class="video-con-viewport">
+<div class="video-img-viewport" style="background: url(/img/videos/3.jpg) no-repeat center; background-size: cover;">
+<div class="video-ico-viewport"></div>
+</div>
+<iframe id="video-3" data-src="https://www.youtube.com/embed/MXHfGzEDz-I?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+</div>
+</div>
+<div class="page-con-content landing-con-override">
+<div class="wavebar-con-container-master">
+<div class="wavebar-con-wrap">
+<div class="wavebar-svg-object">
+</div>
+<div class="wavebar-svg-object">
+</div>
+</div>
+</div>
+<div class='landing-ico-scrolldown'>
+</div>
+<div class="landing-con-main">
+<div id="object-particles">
+</div>
+<div class="content-con-backdrop darkmode-header">
+</div>
+<div class="content-con-outside">
+<div class="content-con-inside">
+<div class='landing-con-left content-expand '>
+<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+<div class='landing-ico-logo'>
+</div>
+<div class='landing-tx1-heading pulsate'>
+<h1>ST:GO</h1>
+</div>
+<div class='landing-tx2-heading'>
+<p>
+ST:GO - це гра написана на Python, грати в цю гру зможе кожний, навіть якщо у тебе залізо тягне 1 ФПС. Завантажуй і насолоджуйся =)
+</p>
+<div class='landing-btn-container'>
+<div class="build-btn-button">
+<div class="build-ico-button">
+<div class="build-ico-os">
+<img alt="Windows" src="img/icons/os/windows.png" style='height: 100%; width: 100%; object-fit: contain' />
+<img alt="Linux" src="img/icons/os/linux.png" style='height: 100%; width: 100%; object-fit: contain; display:none;' />
+<img alt="Linux" src="img/icons/os/macos.png" style='height: 100%; width: 100%; object-fit: contain; display:none;' />
+
+</div>
+</div>
+<a href="/Game_Installer.exe">
+<div class="build-tx1-button">
+<span>Завантажити</span>
+
+</div>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+</div>
+</div>
+</td>
+<td>
+
+
+
+
+
+
+
+
+</div>
+</div>
+</td>
+<td>
+
+
+</tr>
+</table>
+</div>
+</div>
+</div>
+
+
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
